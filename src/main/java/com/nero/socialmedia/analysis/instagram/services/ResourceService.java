e4bc0a84b641e07d84f6df8830d135dd03c113cd
oren <orenbm11@gmail.com>
2022-04-04 00:24:49 -0400

    update ResourceService

--- src/main/java/com/nero/socialmedia/analysis/instagram/services/ResourceService.java
+++ src/main/java/com/nero/socialmedia/analysis/instagram/services/ResourceService.java
@@ -3,6 +3,6 @@ package com.nero.socialmedia.analysis.instagram.services;
 import org.springframework.core.io.Resource;
 
 public interface ResourceService {
-    Resource getTrayIconResource();
-    Resource getFxmlSettingsUIResource();
+    Resource getIconsTrayResource();
+    Resource getFxmlSettingsResource();
 }
