ebda1ade026af64bd9c4079baa408d22fd5a7449
oren <orenbm11@gmail.com>
2022-04-04 02:32:17 -0400

    tweak

--- src/main/java/com/nero/socialmedia/analysis/instagram/configuration/dialect/SQLDialect.java
+++ src/main/java/com/nero/socialmedia/analysis/instagram/configuration/dialect/SQLDialect.java
@@ -57,10 +57,12 @@ public class SQLDialect extends Dialect {
         return "select last_insert_rowid()";
     }
 
+    @Override
     public boolean supportsLimit() {
         return true;
     }
 
+    @Override
     protected String getLimitString(String query, boolean hasOffset) {
         return query + (hasOffset ? " limit ? offset ?" : " limit ?");
     }
@@ -77,59 +79,73 @@ public class SQLDialect extends Dialect {
         return false;
     }
 
+    @Override
     public boolean supportsCurrentTimestampSelection() {
         return true;
     }
 
+    @Override
     public boolean isCurrentTimestampSelectStringCallable() {
         return false;
     }
 
+    @Override
     public String getCurrentTimestampSelectString() {
         return "select current_timestamp";
     }
 
+    @Override
     public boolean supportsUnionAll() {
         return true;
     }
 
+    @Override
     public boolean hasAlterTable() {
         return false; // As specify in NHibernate dialect
     }
 
+    @Override
     public boolean dropConstraints() {
         return false;
     }
 
+    @Override
     public String getAddColumnString() {
         return "add column";
     }
 
+    @Override
     public String getForUpdateString() {
         return "";
     }
 
+    @Override
     public boolean supportsOuterJoinForUpdate() {
         return false;
     }
 
+    @Override
     public String getDropForeignKeyString() {
         throw new UnsupportedOperationException("No drop foreign key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable,
                                                    String[] primaryKey, boolean referencesPrimaryKey) {
         throw new UnsupportedOperationException("No add foreign key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public String getAddPrimaryKeyConstraintString(String constraintName) {
         throw new UnsupportedOperationException("No add primary key syntax supported by SQLiteDialect");
     }
 
+    @Override
     public boolean supportsIfExistsBeforeTableName() {
         return true;
     }
 
+    @Override
     public boolean supportsCascadeDelete() {
         return false;
     }
